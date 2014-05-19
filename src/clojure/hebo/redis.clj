980201bfb60e398da8fc98a4bc5b67ffcfc67335
liujiacai <jiacai2050@gmail.com>
2014-05-19 21:31:22 +0800

    change redis default port from 9876 to 6379

--- src/clojure/hebo/redis.clj
+++ src/clojure/hebo/redis.clj
@@ -8,7 +8,7 @@
 (defn get-conn []
   (let [default-name (parse-default-name (resource "core-site.xml"))
         ip (nth (split default-name #":|//") 2)
-        port  (Integer. (or (System/getenv "HEBO_PORT") 9876))]
+        port  (Integer. (or (System/getenv "HEBO_PORT") 6379))]
     {:pool {} :spec {:host ip :port port}}))
 
 (defmacro redis [& body] `(car/wcar (get-conn) ~@body))
